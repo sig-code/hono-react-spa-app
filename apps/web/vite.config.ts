4bd6ee57e7ae2506ea8f8b89cded6a891b13b34d
mashimo takuya <t_mashimo@uluru.jp>
2025-04-14 14:25:23 +0900

    📈 Biome設定の改善とlefthookの対象ファイル拡張子追加

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,23 +1,23 @@
-import path from 'node:path'
-import react from '@vitejs/plugin-react'
-import postcssPresetMantine from 'postcss-preset-mantine'
-import postcssSimpleVars from 'postcss-simple-vars'
-import { defineConfig } from 'vite'
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import postcssPresetMantine from "postcss-preset-mantine";
+import postcssSimpleVars from "postcss-simple-vars";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@repo': path.resolve(__dirname, '../../packages'),
+      "@src": path.resolve(__dirname, "./src"),
+      "@repo": path.resolve(__dirname, "../../packages"),
     },
   },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8787',
+      "/api": {
+        target: "http://localhost:8787",
         changeOrigin: true,
       },
     },
@@ -25,7 +25,7 @@ export default defineConfig({
   css: {
     // CSSモジュールの設定
     modules: {
-      localsConvention: 'camelCase',
+      localsConvention: "camelCase",
     },
     // PostCSSの設定
     postcss: {
@@ -33,14 +33,14 @@ export default defineConfig({
         postcssPresetMantine(),
         postcssSimpleVars({
           variables: {
-            'mantine-breakpoint-xs': '36em',
-            'mantine-breakpoint-sm': '48em',
-            'mantine-breakpoint-md': '62em',
-            'mantine-breakpoint-lg': '75em',
-            'mantine-breakpoint-xl': '88em',
+            "mantine-breakpoint-xs": "36em",
+            "mantine-breakpoint-sm": "48em",
+            "mantine-breakpoint-md": "62em",
+            "mantine-breakpoint-lg": "75em",
+            "mantine-breakpoint-xl": "88em",
           },
         }),
       ],
     },
   },
-})
+});
