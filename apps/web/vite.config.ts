3cc8449e4beb7a70c5b9e58946ae481c6835b49a
mashimo takuya <t_mashimo@uluru.jp>
2025-04-22 17:15:18 +0900

    🚀 マルチ環境対応の設定追加

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,46 +5,48 @@ import postcssSimpleVars from "postcss-simple-vars";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@src": path.resolve(__dirname, "./src"),
-      "@repo": path.resolve(__dirname, "../../packages"),
-    },
-  },
-  build: {
-    outDir: "../../dist/apps/web",
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8787",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@src": path.resolve(__dirname, "./src"),
+        "@repo": path.resolve(__dirname, "../../packages"),
       },
     },
-  },
-  css: {
-    // CSSモジュールの設定
-    modules: {
-      localsConvention: "camelCase",
+    build: {
+      outDir: "../../dist/apps/web",
+      emptyOutDir: true,
     },
-    // PostCSSの設定
-    postcss: {
-      plugins: [
-        postcssPresetMantine(),
-        postcssSimpleVars({
-          variables: {
-            "mantine-breakpoint-xs": "36em",
-            "mantine-breakpoint-sm": "48em",
-            "mantine-breakpoint-md": "62em",
-            "mantine-breakpoint-lg": "75em",
-            "mantine-breakpoint-xl": "88em",
-          },
-        }),
-      ],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_BASE_URL || "http://localhost:8787",
+          changeOrigin: true,
+        },
+      },
     },
-  },
+    css: {
+      // CSSモジュールの設定
+      modules: {
+        localsConvention: "camelCase",
+      },
+      // PostCSSの設定
+      postcss: {
+        plugins: [
+          postcssPresetMantine(),
+          postcssSimpleVars({
+            variables: {
+              "mantine-breakpoint-xs": "36em",
+              "mantine-breakpoint-sm": "48em",
+              "mantine-breakpoint-md": "62em",
+              "mantine-breakpoint-lg": "75em",
+              "mantine-breakpoint-xl": "88em",
+            },
+          }),
+        ],
+      },
+    },
+  };
 });
